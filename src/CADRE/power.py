e3d02ab2db32ac5fa5712c9ed21b14b51942cfab
Laurynas Mačiulis <aviatorius@gmail.com>
2017-06-06 00:10:55 +0300

    initial commit

--- src/CADRE/power.py
+++ src/CADRE/power.py
@@ -1,155 +1,148 @@
-''' Power discipline for CADRE '''
+''' Adaptation of simplified power discipline for LS2 based on CADRE '''
 
-import os
 from six.moves import range
 
 import numpy as np
 
 from openmdao.core.component import Component
 
-from MBI import MBI
+# class Power_CellVoltage(Component):
+    # """Compute the output voltage of the solar panels.
+    # """
 
-# Allow non-standard variable names for scientific calc
-# pylint: disable-msg=C0103
+    # def __init__(self, n, dat=None):
+        # super(Power_CellVoltage, self).__init__()
 
+        # self.n = n
 
-class Power_CellVoltage(Component):
-    """Compute the output voltage of the solar panels.
-    """
+        # if dat is None:
+            # fpath = os.path.dirname(os.path.realpath(__file__))
+            # dat = np.genfromtxt(fpath + '/data/Power/curve.dat')
 
-    def __init__(self, n, dat=None):
-        super(Power_CellVoltage, self).__init__()
+        # # Inputs
+        # self.add_param('LOS', np.zeros((n)), units='unitless',
+                       # desc="Line of Sight over Time")
 
-        self.n = n
+        # self.add_param('temperature', 273.*np.ones((5, n)), units="degK",
+                       # desc="Temperature of solar cells over time")
 
-        if dat is None:
-            fpath = os.path.dirname(os.path.realpath(__file__))
-            dat = np.genfromtxt(fpath + '/data/Power/curve.dat')
+        # self.add_param('exposedArea', np.zeros((7, 12, n)), units="m**2",
+                       # desc="Exposed area to sun for each solar cell over time")
 
-        # Inputs
-        self.add_param('LOS', np.zeros((n)), units='unitless',
-                       desc="Line of Sight over Time")
+        # self.add_param('Isetpt', np.zeros((12, n)), units="A",
+                       # desc="Currents of the solar panels")
 
-        self.add_param('temperature', 273.*np.ones((5, n)), units="degK",
-                       desc="Temperature of solar cells over time")
+        # # Outputs
+        # self.add_output('V_sol', np.zeros((12, n)), units="V",
+                        # desc="Output voltage of solar panel over time")
 
-        self.add_param('exposedArea', np.zeros((7, 12, n)), units="m**2",
-                       desc="Exposed area to sun for each solar cell over time")
+        # nT, nA, nI = dat[:3]
+        # nT = int(nT)
+        # nA = int(nA)
+        # nI = int(nI)
+        # T = dat[3:3 + nT]
+        # A = dat[3 + nT:3 + nT + nA]
+        # I = dat[3 + nT + nA:3 + nT + nA + nI]
+        # V = dat[3 + nT + nA + nI:].reshape((nT, nA, nI), order='F')
 
-        self.add_param('Isetpt', np.zeros((12, n)), units="A",
-                       desc="Currents of the solar panels")
+        # self.MBI = MBI(V, [T, A, I], [6, 6, 15], [3, 3, 3])
 
-        # Outputs
-        self.add_output('V_sol', np.zeros((12, n)), units="V",
-                        desc="Output voltage of solar panel over time")
+        # self.x = np.zeros((84 * self.n, 3), order='F')
+        # self.xV = self.x.reshape((self.n, 7, 12, 3), order='F')
+        # self.dV_dL = np.zeros((self.n, 12), order='F')
+        # self.dV_dT = np.zeros((self.n, 12, 5), order='F')
+        # self.dV_dA = np.zeros((self.n, 7, 12), order='F')
+        # self.dV_dI = np.zeros((self.n, 12), order='F')
 
-        nT, nA, nI = dat[:3]
-        nT = int(nT)
-        nA = int(nA)
-        nI = int(nI)
-        T = dat[3:3 + nT]
-        A = dat[3 + nT:3 + nT + nA]
-        I = dat[3 + nT + nA:3 + nT + nA + nI]
-        V = dat[3 + nT + nA + nI:].reshape((nT, nA, nI), order='F')
+    # def setx(self, params):
 
-        self.MBI = MBI(V, [T, A, I], [6, 6, 15], [3, 3, 3])
+        # temperature = params['temperature']
+        # LOS = params['LOS']
+        # exposedArea = params['exposedArea']
+        # Isetpt = params['Isetpt']
 
-        self.x = np.zeros((84 * self.n, 3), order='F')
-        self.xV = self.x.reshape((self.n, 7, 12, 3), order='F')
-        self.dV_dL = np.zeros((self.n, 12), order='F')
-        self.dV_dT = np.zeros((self.n, 12, 5), order='F')
-        self.dV_dA = np.zeros((self.n, 7, 12), order='F')
-        self.dV_dI = np.zeros((self.n, 12), order='F')
+        # for p in range(12):
+            # i = 4 if p < 4 else (p % 4)
+            # for c in range(7):
+                # self.xV[:, c, p, 0] = temperature[i, :]
+                # self.xV[:, c, p, 1] = LOS * exposedArea[c, p, :]
+                # self.xV[:, c, p, 2] = Isetpt[p, :]
 
-    def setx(self, params):
+    # def solve_nonlinear(self, params, unknowns, resids):
+        # """ Calculate output. """
 
-        temperature = params['temperature']
-        LOS = params['LOS']
-        exposedArea = params['exposedArea']
-        Isetpt = params['Isetpt']
+        # self.setx(params)
+        # self.raw = self.MBI.evaluate(self.x)[:, 0].reshape((self.n, 7, 12),
+                                                           # order='F')
+        # unknowns['V_sol'] = np.zeros((12, self.n))
+        # for c in range(7):
+            # unknowns['V_sol'] += self.raw[:, c, :].T
 
-        for p in range(12):
-            i = 4 if p < 4 else (p % 4)
-            for c in range(7):
-                self.xV[:, c, p, 0] = temperature[i, :]
-                self.xV[:, c, p, 1] = LOS * exposedArea[c, p, :]
-                self.xV[:, c, p, 2] = Isetpt[p, :]
+    # def linearize(self, params, unknowns, resids):
+        # """ Calculate and save derivatives. (i.e., Jacobian) """
 
-    def solve_nonlinear(self, params, unknowns, resids):
-        """ Calculate output. """
+        # exposedArea = params['exposedArea']
+        # LOS = params['LOS']
 
-        self.setx(params)
-        self.raw = self.MBI.evaluate(self.x)[:, 0].reshape((self.n, 7, 12),
-                                                           order='F')
-        unknowns['V_sol'] = np.zeros((12, self.n))
-        for c in range(7):
-            unknowns['V_sol'] += self.raw[:, c, :].T
+        # self.raw1 = self.MBI.evaluate(self.x, 1)[:, 0].reshape((self.n, 7,
+                                                                # 12), order='F')
+        # self.raw2 = self.MBI.evaluate(self.x, 2)[:, 0].reshape((self.n, 7,
+                                                                # 12), order='F')
+        # self.raw3 = self.MBI.evaluate(self.x, 3)[:, 0].reshape((self.n, 7,
+                                                                # 12), order='F')
+        # self.dV_dL[:] = 0.0
+        # self.dV_dT[:] = 0.0
+        # self.dV_dA[:] = 0.0
+        # self.dV_dI[:] = 0.0
 
-    def linearize(self, params, unknowns, resids):
-        """ Calculate and save derivatives. (i.e., Jacobian) """
+        # for p in range(12):
+            # i = 4 if p < 4 else (p % 4)
+            # for c in range(7):
+                # self.dV_dL[:, p] += self.raw2[:, c, p] * exposedArea[c, p,:]
+                # self.dV_dT[:, p, i] += self.raw1[:, c, p]
+                # self.dV_dA[:, c, p] += self.raw2[:, c, p] * LOS
+                # self.dV_dI[:, p] += self.raw3[:, c, p]
 
-        exposedArea = params['exposedArea']
-        LOS = params['LOS']
+    # def apply_linear(self, params, unknowns, dparams, dunknowns, dresids, mode):
+        # """ Matrix-vector product with the Jacobian. """
 
-        self.raw1 = self.MBI.evaluate(self.x, 1)[:, 0].reshape((self.n, 7,
-                                                                12), order='F')
-        self.raw2 = self.MBI.evaluate(self.x, 2)[:, 0].reshape((self.n, 7,
-                                                                12), order='F')
-        self.raw3 = self.MBI.evaluate(self.x, 3)[:, 0].reshape((self.n, 7,
-                                                                12), order='F')
-        self.dV_dL[:] = 0.0
-        self.dV_dT[:] = 0.0
-        self.dV_dA[:] = 0.0
-        self.dV_dI[:] = 0.0
+        # dV_sol = dresids['V_sol']
 
-        for p in range(12):
-            i = 4 if p < 4 else (p % 4)
-            for c in range(7):
-                self.dV_dL[:, p] += self.raw2[:, c, p] * exposedArea[c, p,:]
-                self.dV_dT[:, p, i] += self.raw1[:, c, p]
-                self.dV_dA[:, c, p] += self.raw2[:, c, p] * LOS
-                self.dV_dI[:, p] += self.raw3[:, c, p]
+        # if mode == 'fwd':
 
-    def apply_linear(self, params, unknowns, dparams, dunknowns, dresids, mode):
-        """ Matrix-vector product with the Jacobian. """
+            # if 'LOS' in dparams:
+                # dV_sol += self.dV_dL.T * dparams['LOS']
 
-        dV_sol = dresids['V_sol']
+            # if 'temperature' in dparams:
+                # for p in range(12):
+                    # i = 4 if p < 4 else (p % 4)
+                    # dV_sol[p, :] += self.dV_dT[:, p, i] * dparams['temperature'][i,:]
 
-        if mode == 'fwd':
+            # if 'Isetpt' in dparams:
+                # dV_sol += self.dV_dI.T * dparams['Isetpt']
 
-            if 'LOS' in dparams:
-                dV_sol += self.dV_dL.T * dparams['LOS']
+            # if 'exposedArea' in dparams:
+                # for p in range(12):
+                    # dV_sol[p, :] += \
+                        # np.sum(self.dV_dA[:,:, p] * dparams['exposedArea'][:, p,:].T, 1)
 
-            if 'temperature' in dparams:
-                for p in range(12):
-                    i = 4 if p < 4 else (p % 4)
-                    dV_sol[p, :] += self.dV_dT[:, p, i] * dparams['temperature'][i,:]
+        # else:
+            # for p in range(12):
+                # i = 4 if p < 4 else (p % 4)
 
-            if 'Isetpt' in dparams:
-                dV_sol += self.dV_dI.T * dparams['Isetpt']
+                # if 'LOS' in dparams:
+                    # dparams['LOS'] += self.dV_dL[:, p] * dV_sol[p,:]
 
-            if 'exposedArea' in dparams:
-                for p in range(12):
-                    dV_sol[p, :] += \
-                        np.sum(self.dV_dA[:,:, p] * dparams['exposedArea'][:, p,:].T, 1)
+                # if 'temperature' in dparams:
+                    # dparams['temperature'][i,:] += self.dV_dT[:, p, i] * dV_sol[p,:]
 
-        else:
-            for p in range(12):
-                i = 4 if p < 4 else (p % 4)
+                # if 'Isetpt' in dparams:
+                    # dparams['Isetpt'][p,:] += self.dV_dI[:, p] * dV_sol[p,:]
 
-                if 'LOS' in dparams:
-                    dparams['LOS'] += self.dV_dL[:, p] * dV_sol[p,:]
-
-                if 'temperature' in dparams:
-                    dparams['temperature'][i,:] += self.dV_dT[:, p, i] * dV_sol[p,:]
-
-                if 'Isetpt' in dparams:
-                    dparams['Isetpt'][p,:] += self.dV_dI[:, p] * dV_sol[p,:]
-
-                if 'exposedArea' in dparams:
-                    dexposedArea = dparams['exposedArea']
-                    for c in range(7):
-                        dexposedArea[c, p, :] += self.dV_dA[:, c, p] * dV_sol[p,:]
+                # if 'exposedArea' in dparams:
+                    # dexposedArea = dparams['exposedArea']
+                    # for c in range(7):
+                        # dexposedArea[c, p, :] += self.dV_dA[:, c, p] * dV_sol[p,:]
 
 
 class Power_SolarPower(Component):
@@ -159,50 +152,48 @@ class Power_SolarPower(Component):
         super(Power_SolarPower, self).__init__()
 
         self.n = n
+		
+		#Constants
+		#LS2 solar array efficiency
+		eff = 0.17
+		
+		#area reduction coefficient to account for different soler cell size on LS2 compared to CADRE
+		red = (0.0116/7)/0.0027
+		
+		#solar constant, W/m^2
+		P_sun = 1365.0
 
         # Inputs
-        self.add_param('Isetpt', np.zeros((12, n)), units="A",
-                       desc="Currents of the solar panels")
+         self.add_param('LOS', np.zeros((n)), units='unitless',
+                       desc="Line of Sight over Time")
 
-        self.add_param('V_sol', np.zeros((12, n)), units="V",
-                       desc="Output voltage of solar panel over time")
+        #self.add_param('temperature', 273.*np.ones((5, n)), units="degK",
+        #               desc="Temperature of solar cells over time")
 
+        self.add_param('exposedArea', np.zeros((7, 12, n)), units="m**2",
+                       desc="Exposed area to sun for each solar cell over time")
+        
         self.add_output('P_sol', np.zeros((n, )), units="W",
                         desc="Solar panels power over time")
 
     def solve_nonlinear(self, params, unknowns, resids):
         """ Calculate output. """
 
-        V_sol = params['V_sol']
-        Isetpt = params['Isetpt']
-        P_sol = unknowns['P_sol']
+        A = params['exposedArea']
+        A_tot = np.zeros((n, ))   
+	    for i in range(n):
+		    A_tot[i] = np.sum(A[:, :7, i])
+			
+        unknowns['P_sol'] = A_tot*P_sun*LOS*eff*red
 
-        P_sol[:] = np.zeros((self.n, ))
-        for p in range(12):
-            P_sol += V_sol[p, :] * Isetpt[p, :]
+    def linearize(self, params, unknowns, resids):
+        """ Jacobian"""
+        J = {}
 
-    def apply_linear(self, params, unknowns, dparams, dunknowns, dresids, mode):
-        """ Matrix-vector product with the Jacobian. """
+        J['P_sol','LOS'] = np.ones((n, ))*A_tot*P_sun*eff*red
+        J['P_sol','exposedArea'] = np.ones((7, 12, n))*P_sun*LOS*eff*red
 
-        dP_sol = dresids['P_sol']
-
-        if mode == 'fwd':
-            if 'V_sol' in dparams:
-                for p in range(12):
-                    dP_sol += dparams['V_sol'][p, :] * params['Isetpt'][p, :]
-
-            if 'Isetpt' in dparams:
-                for p in range(12):
-                    dP_sol += dparams['Isetpt'][p, :] * params['V_sol'][p, :]
-
-        else:
-            for p in range(12):
-                if 'V_sol' in dparams:
-                    dparams['V_sol'][p,:] += dP_sol* params['Isetpt'][p, :]
-
-                if 'Isetpt' in dparams:
-                    dparams['Isetpt'][p,:] += params['V_sol'][p, :] * dP_sol
-
+        return J
 
 class Power_Total(Component):
     """ Compute the battery power which is the sum of the loads. This
